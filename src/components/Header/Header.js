4156af78fc505ad2fd0e1fc43f9b7d9524181baf
webexpertsabbir <webexpertsabbir@gmail.com>
2022-10-21 17:16:51 +0600

    login form

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../Context/UserContext';
 import logo from '../../images/Logo.svg';
 import './Header.css';
 
 const Header = () => {
+    const {user} = useContext(AuthContext);
+    
     return (
         <nav className='header'>
             <img src={logo} alt="" />
@@ -14,6 +17,7 @@ const Header = () => {
                 <Link to="/about">About</Link>
                 <Link to='/login'>Log In</Link>
                 <Link to='/signup'>Sign Up</Link>
+                <span>{user?.email}</span>
             </div>
         </nav>
     );
